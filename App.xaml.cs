e997c2395fdd859c02ee3e374dca2ef93d82e6bc
Marianolaola <laolamariano@gmail.com>
2025-08-31 20:14:48 -0300

    Ventana Login

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,12 @@ namespace Sistema_de_Gestión_Farmacéutica
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
     }
 
 }
---
+++ Productos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Gestión_Farmacéutica
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
